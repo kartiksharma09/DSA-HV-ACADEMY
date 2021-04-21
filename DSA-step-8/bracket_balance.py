4990b2f5e8c7af34f3da18753fc501d957e316f6
kartik sharma <64221628+kartiksharma09@users.noreply.github.com>
2021-04-22 02:24:08 +0530

    Update bracket_balance.py

--- DSA-step-8/bracket_balance.py
+++ DSA-step-8/bracket_balance.py
@@ -1,30 +1,21 @@
 def isBalanced(brackets):
-
 	stack = []
 	brackets = input()
 
 	for i in brackets:
 		if i in ["(","[","{"]:
 			stack.append(i)
-
 		else:
 			if len(stack)==0:
 				return False
-
 			else:
 				last_ele = stack.pop()
 
-
 			if last_ele == "(" and i!=")":
-
 				return False
-
 			if last_ele == "[" and i!="]":
-
 				return False
-
 			if last_ele == "{" and i!="}":
-
 				return False
 
 	if len(stack)>0:
