179a103e8cd834534918df2cb9664275bac7e40f
kartik sharma <kartik19@navgurukul.org>
2021-04-21 23:33:04 +0530

    stack_using_array added

---
+++ DSA-step-8/stack_using_array.py
@@ -0,0 +1,50 @@
+class Mystack:
+
+	def __init__(self):
+
+		self.stack = []
+
+	def isempty(self):
+
+		return len(self.stack)==0
+
+	def push(self,data):
+
+		self.stack.append(data)
+
+	def pop(self):
+
+		if len(self.stack)!=0:
+
+			self.stack.pop()
+
+		else:
+
+			print("stack is empty")
+
+	def peek(self):
+
+		if len(self.stack)!=0:
+
+			return self.stack[-1]
+
+		else:
+
+			print("stack is empty")
+
+
+	def show(self):
+
+		print(self.stack)
+
+	def length(self):
+
+		return len(self.stack)
+
+stack_op = Mystack()
+
+stack_op.push(1)
+stack_op.show()
+stack_op.pop()
+stack_op.show()
+stack_op.peek()
