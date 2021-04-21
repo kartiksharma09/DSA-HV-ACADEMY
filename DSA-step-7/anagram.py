7185777eb2961794d571a6da9b4baab9b916e0a9
kartik sharma <kartik19@navgurukul.org>
2021-04-22 00:04:59 +0530

    updated_anagram

--- DSA-step-7/anagram.py
+++ DSA-step-7/anagram.py
@@ -1,10 +1,39 @@
-user1 = "silent"
-user2 = "listen"
 
-str1 = sorted(user1)
-str2 = sorted(user2)
+str1 = input()
+str2 = input()
 
-if str1 == str2:
-	print("Anagram")
+dict1 = {}
+dict2 = {}
+
+if len(str1)==len(str2):
+
+	for i in range(len(str1)):
+
+		if str1[i] not in dict1:
+
+			dict1[str1[i]]=1
+
+		else:
+
+			dict1[str1[i]]+=1
+
+
+		if str2[i] not in dict2:
+
+			dict2[str2[i]]=1
+
+		else:
+
+			dict2[str2[i]]+=1
+
+	if dict1 == dict2:
+
+		print("anagram")
+
+	else:
+
+		print("not an anagram")
+
 else:
-	print("Not Anagram")
+
+	print("not an anagram")
