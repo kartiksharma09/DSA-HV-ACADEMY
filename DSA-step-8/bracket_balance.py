16d329b2d9ae2ef367439f0104527d9d5f2a938c
kartik sharma <kartik19@navgurukul.org>
2021-04-22 02:22:35 +0530

    Bracket_matching added

---
+++ DSA-step-8/bracket_balance.py
@@ -0,0 +1,37 @@
+def isBalanced(brackets):
+
+	stack = []
+	brackets = input()
+
+	for i in brackets:
+		if i in ["(","[","{"]:
+			stack.append(i)
+
+		else:
+			if len(stack)==0:
+				return False
+
+			else:
+				last_ele = stack.pop()
+
+
+			if last_ele == "(" and i!=")":
+
+				return False
+
+			if last_ele == "[" and i!="]":
+
+				return False
+
+			if last_ele == "{" and i!="}":
+
+				return False
+
+	if len(stack)>0:
+		return False
+	return True
+
+if isBalanced(brackets):
+	print("Brackets are Balanced")
+else:
+	print("Brackets aren't Balanced")
