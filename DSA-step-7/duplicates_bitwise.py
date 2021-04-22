e623fd45d9137712b7edf4bcaaebce35bd112c24
kartik sharma <kartik19@navgurukul.org>
2021-04-22 14:48:37 +0530

    added new question

---
+++ DSA-step-7/duplicates_bitwise.py
@@ -0,0 +1,10 @@
+a = input()
+duplicates = ""
+
+for i in range(len(a)-1):
+	for j in range(i+1,len(a)):
+		if (ord(a[i])^ord(a[j]))==0:
+			if a[j] not in duplicates:
+				duplicates=duplicates+" "+a[j]
+			
+print("duplicates are",duplicates)
