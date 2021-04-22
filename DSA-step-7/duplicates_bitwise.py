54372aee903cccd698a913446d04ef02aba5a9b6
kartik sharma <64221628+kartiksharma09@users.noreply.github.com>
2021-04-22 17:54:12 +0530

    Update duplicates_bitwise.py

--- DSA-step-7/duplicates_bitwise.py
+++ DSA-step-7/duplicates_bitwise.py
@@ -3,7 +3,9 @@ duplicates = ""
 
 for i in range(len(a)-1):
 	for j in range(i+1,len(a)):
-		if (ord(a[i])^ord(a[j]))==0:
+		if a[i] in duplicates:
+			break
+		elif (ord(a[i])^ord(a[j]))==0:
 			if a[j] not in duplicates:
 				duplicates=duplicates+" "+a[j]
 			
