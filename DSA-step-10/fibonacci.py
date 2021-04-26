6272f0c208ebeaf0fe621398818951cdba59cbf8
kartik sharma <kartik19@navgurukul.org>
2021-04-26 16:19:58 +0530

    updated fibonacci

--- DSA-step-10/fibonacci.py
+++ DSA-step-10/fibonacci.py
@@ -1,7 +1,12 @@
+fib_memo = {}
 def fibonacci(n):
+	if n in fib_memo:
+		return fib_memo[n]
 	if n<2:
 		return n
-	else:
-		return fibonacci(n-1)+fibonacci(n-2)
+	elif n>=2:
+		fib_value= fibonacci(n-1)+fibonacci(n-2)
+	fib_memo[n]=fib_value
+	return fib_value	
 
-print(fibonacci(3))
+print(fibonacci(9))
